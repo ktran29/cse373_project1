0516da1cfff3030ed29dc0d497bc071868e64863
marcu <marcu@69.91.137.25>
2017-10-04 10:15:19 -0700

    Only null key issue remains for arrayDictionary

--- src/main/java/datastructures/concrete/dictionaries/ArrayDictionary.java
+++ src/main/java/datastructures/concrete/dictionaries/ArrayDictionary.java
@@ -43,7 +43,7 @@ public class ArrayDictionary<K, V> implements IDictionary<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < pairs.length; i++) {
-            if(pairs[i] != null && pairs[i].key == key) {
+            if(pairs[i] != null && pairs[i].key.equals(key)) {
                 return pairs[i].value;
             }
         }
@@ -83,7 +83,7 @@ public class ArrayDictionary<K, V> implements IDictionary<K, V> {
     @Override
     public V remove(K key) {
         for (int i = 0; i < pairs.length; i++) {
-            if (pairs[i] != null && pairs[i].key == key) {
+            if (pairs[i] != null && pairs[i].key.equals(key)) {
                 V removedValue = pairs[i].value;
                 pairs[i] = null;
                 for (int j = i; j < pairs.length - 1; j++) {
@@ -99,7 +99,7 @@ public class ArrayDictionary<K, V> implements IDictionary<K, V> {
     @Override
     public boolean containsKey(K key) {
         for (int i = 0; i < pairs.length; i++) {
-            if(pairs[i] != null && pairs[i].key == key) {
+            if(pairs[i] != null && pairs[i].key.equals(key)) {
                 return true;
             }
         }
